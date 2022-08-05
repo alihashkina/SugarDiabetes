53a5b17a32a39c395619a0dd463c18e4de4b41d8
alihashkina <85725433+alihashkina@users.noreply.github.com>
2022-08-05 13:41:29 +0300

    f

--- app/src/main/java/com/bignerdranch/android/sugardiabetes/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sugardiabetes/MainActivity.kt
@@ -9,33 +9,33 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     companion object{
-        lateinit var menu: BottomNavigationView
+       // lateinit var menu: BottomNavigationView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        menu = findViewById(R.id.menu)
-        menu.setOnNavigationItemSelectedListener {
-                item ->
-            when (item.itemId) {
-                R.id.generalPage -> {
-                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, ListOfApplicationsAll.newInstance()).addToBackStack(null).commit()
-                }
-                R.id.calendar -> {
-                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, ListEquipment.newInstance()).addToBackStack(null).commit()
-                }
-                R.id.statistic -> {
-                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, GeneralPage.newInstance()).addToBackStack(null).commit()
-                }
-                R.id.setting -> {
-                    // supportFragmentManager.beginTransaction().replace(R.id.containerView, GeneralPage.newInstance()).addToBackStack(null).commit()
-                }
-
-            }
-            true
-        }
+//        menu = findViewById(R.id.menu)
+//        menu.setOnNavigationItemSelectedListener {
+//                item ->
+//            when (item.itemId) {
+//                R.id.generalPage -> {
+//                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, ListOfApplicationsAll.newInstance()).addToBackStack(null).commit()
+//                }
+//                R.id.calendar -> {
+//                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, ListEquipment.newInstance()).addToBackStack(null).commit()
+//                }
+//                R.id.statistic -> {
+//                   // supportFragmentManager.beginTransaction().replace(R.id.containerView, GeneralPage.newInstance()).addToBackStack(null).commit()
+//                }
+//                R.id.setting -> {
+//                    // supportFragmentManager.beginTransaction().replace(R.id.containerView, GeneralPage.newInstance()).addToBackStack(null).commit()
+//                }
+//
+//            }
+//            true
+//        }
 
 
         if (savedInstanceState == null) {
